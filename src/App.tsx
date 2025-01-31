4b12f40a18ca2e815a6526421ff718103ee68d7a
Victor Oliveira <victor.oliveira20@icloud.com>
2025-01-31 10:48:31 -0300

    develop header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Header from "./components";
+
 function App() {
   return (
     <>
-      <h1>Hello, world!</h1>
+      <Header />
     </>
   );
 }
---
+++ src/components/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        <div className="ds-container">DSCarrosTop</div>
+      </header>
+    </>
+  );
+}
